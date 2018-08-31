cb77930ab8c56b6fcf46158c0544cf65749ac95b
Milind Singh <milind7@live.com>
2018-08-31 12:31:08 +0530

    Minor updates

--- tests/BarcodeTest.php
+++ tests/BarcodeTest.php
@@ -14,66 +14,76 @@ class BarcodeTest extends \PHPUnit_Framework_TestCase
 
     public function testInit()
     {
-        $validator = new Barcode('12345123');
+        $validator = new Barcode();
+        $validator->setBarcode('12345123');
         $this->assertInstanceOf('\Ced\Validator\Barcode', $validator);
     }
 
     public function testInvalid()
     {
-        $validator = new Barcode('string123');
+        $validator = new Barcode();
+        $validator->setBarcode('string123');
         $this->assertFalse($validator->isValid());
     }
 
     public function testInvalidLooksLikeBarcode()
     {
-        $validator = new Barcode('5060411950138');
+        $validator = new Barcode();
+        $validator->setBarcode('5060411950138');
         $this->assertFalse($validator->isValid());
     }
 
     public function testValid()
     {
-        $validator = new Barcode('001303050100');
+        $validator = new Barcode();
+        $validator->setBarcode('001303050100');
         $this->assertFalse($validator->isValid());
     }
 
     public function testEanRestricted()
     {
-        $validator = new Barcode('2100000030019');
+        $validator = new Barcode();
+        $validator->setBarcode('2100000030019');
         $this->assertTrue($validator->isValid());
-        $this->assertSame(Barcode::TYPE_EAN_RESTRICTED, $validator->getType());
+        $this->assertSame(Barcode::TYPE_EAN, $validator->getType());
     }
 
     public function testUpcCouponCode()
     {
-        $validator = new Barcode('570691542245');
+        $validator = new Barcode();
+        $validator->setBarcode('570691542245');
         $this->assertTrue($validator->isValid());
         $this->assertSame(Barcode::TYPE_UPC_COUPON_CODE, $validator->getType());
     }
 
     public function testEan()
     {
-        $validator = new Barcode('8838108476586');
+        $validator = new Barcode();
+        $validator->setBarcode('8838108476586');
         $this->assertTrue($validator->isValid());
         $this->assertSame(Barcode::TYPE_EAN, $validator->getType());
     }
 
     public function testEan2()
     {
-        $validator = new Barcode('5060411950139');
+        $validator = new Barcode();
+        $validator->setBarcode('5060411950139');
         $this->assertTrue($validator->isValid());
         $this->assertSame(Barcode::TYPE_EAN, $validator->getType());
     }
 
     public function testEan3()
     {
-        $validator = new Barcode('0700867967774');
+        $validator = new Barcode();
+        $validator->setBarcode('0700867967774');
         $this->assertTrue($validator->isValid());
         $this->assertSame(Barcode::TYPE_EAN, $validator->getType());
     }
 
     public function testUpc()
     {
-        $validator = new Barcode('700867967774');
+        $validator = new Barcode();
+        $validator->setBarcode('700867967774');
         $this->assertTrue($validator->isValid());
         $this->assertSame(Barcode::TYPE_UPC, $validator->getType());
     }
